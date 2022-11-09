aab7ec2ddff8cf28488b9e9f37f271dbd554b2c4
germanpazm <german.paz@educa.madrid.org>
2022-11-09 01:24:45 +0100

    Act ejUD3

--- UD3/ej9_2.js
+++ UD3/ej9_2.js
@@ -1,1 +1,18 @@
 let cadena=prompt("Introduzca la cadena ")
+
+let arraycadena=cadena.split(":");
+let pos=cadena.indexOf("@");
+
+arraycadena.forEach(function  (e){
+    if(pos==-1){
+        console.log(e);
+    }
+    else{
+        let cadenamod=e.substr(0,pos);
+        console.log(cadenamod);
+
+
+
+    }
+})
+
