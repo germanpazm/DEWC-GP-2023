8670658b648bb318934cd14263d078ade4cc147d
germanpazm <german.paz@educa.madrid.org>
2023-02-02 14:12:07 +0100

    Vue

---
+++ vueJs/vueJs/Plantilla-Ramona/src/paginas/paginaListar.vue
@@ -0,0 +1,18 @@
+<script setup>
+import contenido from '../components/contenido.vue'
+</script>
+
+<template>
+    <div>
+        <h1>Página Listar</h1>
+        <div>fasdfasdfasdfasdfasdfasdfasdfasdffa </div>
+        <div>fasdfasdfasdfasdfasdfasdfasdfasdffa </div>
+        
+        <contenido parametro="x"/>
+
+    </div>
+</template>
+
+<style scoped>
+
+</style>
