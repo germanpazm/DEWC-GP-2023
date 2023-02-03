5d7fe18bd4c98d6d7fade143ffa6ee1673b00c80
germanpazm <german.paz@educa.madrid.org>
2023-02-03 12:41:29 +0100

    Vue

--- vueJs/Plantilla-Ramona/src/components/contenido.vue
+++ vueJs/Plantilla-Ramona/src/components/contenido.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref,onMounted } from "vue"
+import { reactive } from "vue";
 import servicioDatosProducto from "../servicios/servicioDatosProducto";
 
 const props = defineProps ({
@@ -8,16 +9,29 @@ const props = defineProps ({
         required: true
     }
 })
-
+let id=0;
 let productos=ref(null);
 let nombreProducto=ref(null);
-// let productoActual=ref(null);
+let productoObj=reactive({ nombre:null,descripcion:null})
 
+let ricky=ref([
+    { id:id++, nombre:"Tierra",creacion:"22"}
+]);
+let i=0;
 function obtenerProductos(){
     servicioDatosProducto.getAll()
     .then ((response) =>{
-        productos.value=response.data;
-        console.log(response.data);
+        let longitud=response.data.results.length;
+
+        while(id<=longitud){
+        
+        ricky.value.push({id:id++,nombre:response.data.results[i].name,creacion:response.data.results[i].created })
+        i++;        
+        console.log(response.data.results[i]);
+        }
+        
+
+        
     })
     .catch((e)=> {
     console.log(e)
@@ -39,14 +53,27 @@ function buscarProducto(){
     }
 }
 
+function crearProducto() {
+
+servicioDatosProducto.create(JSON.stringify(productoObj)).then(response => {
+    obtenerProductos();
+
+    })
+    .catch(e => {
+        console.log(e);
+    })
+}
+
+
+
+
+
 
 /*let data={}
-data.nombre="Cacahuetes"
+data.nombre="Cacahuetes";
 data.descripcion="Descripcion Cacahuetes"
 
-*/
-function insertarProducto(){
-servicioDatosProducto.create(JSON.stringify(nombreProducto.value))
+servicioDatosProducto.create(JSON.stringify(data))
     .then(response => {
        alert("Ha insertado correctamente el elemento")
 
@@ -56,8 +83,8 @@ servicioDatosProducto.create(JSON.stringify(nombreProducto.value))
         alert("Error al insertar");
         });
     
-
-    }
+*/
+    
 
 
 
@@ -70,16 +97,38 @@ onMounted( ()=>{
 <template>
     <input type="text" placeholder="Producto a buscar" v-model="nombreProducto">
     <button type="button" @click="buscarProducto">buscar</button>
-    <button type="button" @click="insertarProducto">Insertar</button>
 
-    <h4>Lista de Productos</h4>
+    <h4>Lista de Localizaciones Ricky Morty</h4>
         <ul>
-            <li v-for="(producto,id) in productos" :key="id">{{ producto.nombre }}</li>
+            <li v-for="todo in ricky" :key="todo.id">
+                {{ todo.nombre }}
+                <br>
+                Fecha de Creacion: {{ todo.creacion }}
+                <p></p>
+                </li>
         </ul>
+        
+        
 
-     <button type="button" @click="obtenerProductos">ListarTdos</button>
-    
-    
+
+     <button type="button" @click="obtenerProductos">ListarTodos</button>
+        <br>
+
+        <h1>insertar</h1>
+        <form @submit.prevent="crearProducto">
+            <label for="idNombre">Nombre</label>
+            <input type="text" v-model="productoObj.nombre" id="idNombre" >
+            <label for="idDescripcion">Descripcion</label>
+            <input type="text" v-model="productoObj.descripcion" id="idDescripcion">
+
+
+            
+            <button type="submit">Crear</button>
+
+
+
+        </form>
+
 </template>
 
 
--- vueJs/Plantilla-Ramona/src/servicios/servicioDatosProducto.js
+++ vueJs/Plantilla-Ramona/src/servicios/servicioDatosProducto.js
@@ -2,23 +2,23 @@ import http from "./http-axios";
 
 class servicioDatosProducto {
   getAll() {
-    return http.get("/productos");
+    return http.get(`/location`);
   }
 
   get(id) {
-    return http.get(`/producto/${id}`);
+    return http.get(`/productos/${id}`);
   }
 
   create(data) {
-    return http.post("/producto", data);
+    return http.post("/productos", data);
   }
 
   update(id, data) {
-    return http.put(`/producto/${id}`, data);
+    return http.put(`/productos/${id}`, data);
   }
 
   delete(id) {
-    return http.delete(`/producto/${id}`);
+    return http.delete(`/productos/${id}`);
   }
 
   deleteAll() {
